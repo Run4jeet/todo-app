1b2ca3744cfdc318f8e7bc8663b145282d1c6a78
run4jeet <sujata.kartikey@gmail.com>
2020-08-13 07:11:26 +0530

    html pages added

--- src/main/java/com/app/todos/todoapp/controller/HomeController.java
+++ src/main/java/com/app/todos/todoapp/controller/HomeController.java
@@ -1,5 +1,24 @@
 package com.app.todos.todoapp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class HomeController {
 
+	@GetMapping("/")
+	public String index() {
+		
+		System.out.println("index page");
+	    return "index";
+	}
+	
+	@GetMapping("/home")
+	public ModelAndView home() {
+		ModelAndView mv= new ModelAndView();
+		mv.setViewName("index.html");//("index");
+		System.out.println("index page2");
+	    return mv;
+	}
 }
