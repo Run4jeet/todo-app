a6581a76a4d4271a8a782e38285de28cdc6cba10
run4jeet <sujata.kartikey@gmail.com>
2020-08-14 07:13:59 +0530

    js and css added

---
+++ todo.js
@@ -0,0 +1,36 @@
+//alert("connected...");
+
+//var firstLi = document.querySelector("li");
+var lis = document.querySelectorAll("li");
+
+for(var i = 0; i< lis.length ;i++){
+
+	lis[i].addEventListener("mouseover" , function(){
+	
+	 this.classList.add("selected");
+	//this.style.color= "green";
+	});
+
+	lis[i].addEventListener("mouseout" , function(){
+
+	this.classList.removed("selected");	
+	//this.style.color= "black";
+	});
+
+	lis[i].addEventListener("click",function(){
+		this.classList.toggle("done");
+
+	})
+
+
+}
+
+/*firstLi.addEventListener("mouseover" , function(){
+firstLi.style.color= "green";
+console.log("mouse over!!");
+});
+
+firstLi.addEventListener("mouseout" ,function(){
+
+	firstLi.style.color="black";
+})*/
